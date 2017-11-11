106863278cd38ba7b534e5d6d3e20fd56a848ba9
TomSun <tftomsun@web.de>
2017-11-12 00:58:24 +0100

    updated scripts switched to RunWebScript function definition

--- AppVeyor/InvokeWebScript.ps1
+++ AppVeyor/InvokeWebScript.ps1
@@ -1,10 +1,12 @@
 ﻿param([String]$scriptFileName, [String]$arguments) #Must be the first statement in your script
 
 $url = 'https://raw.githubusercontent.com/TFTomSun/Environment.GitHub/master/AppVeyor/' + $scriptFileName;
+$scriptFilePath = [io.path]::combine($($env:TEMP), $scriptFileName)
 # Download the script
-Invoke-WebRequest -Uri $url -OutFile $scriptFileName
+
+Invoke-WebRequest -Uri $url -OutFile $scriptFilePath
 # Invoke the script
-Invoke-Expression ".\$scriptFileName $arguments"
+Invoke-Expression "$scriptFilePath $arguments"
 
 #$Script = Invoke-WebRequest $url;
 #$ScriptBlock = [Scriptblock]::Create($Script.Content)
