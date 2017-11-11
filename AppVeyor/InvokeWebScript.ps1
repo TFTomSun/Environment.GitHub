7c6e327c59a268ba1ef9af0a75bc1bc7264bc9a7
TomSun <tftomsun@web.de>
2017-11-11 17:41:22 +0100

    fixed scripts

--- AppVeyor/InvokeWebScript.ps1
+++ AppVeyor/InvokeWebScript.ps1
@@ -1,6 +1,11 @@
-﻿param([String]$scriptFileName) #Must be the first statement in your script
+﻿param([String]$scriptFileName, [String]$arguments) #Must be the first statement in your script
 
 $url = 'https://raw.githubusercontent.com/TFTomSun/Environment.GitHub/master/AppVeyor/' + $scriptFileName;
-$Script = Invoke-WebRequest $url;
-$ScriptBlock = [Scriptblock]::Create($Script.Content)
-Invoke-Command -ScriptBlock $ScriptBlock -ArgumentList ($args + @('someargument'))
+# Download the script
+Invoke-WebRequest -Uri $url -OutFile $scriptFileName
+# Invoke the script
+Invoke-Expression ".\$scriptFileName $arguments"
+
+#$Script = Invoke-WebRequest $url;
+#$ScriptBlock = [Scriptblock]::Create($Script.Content)
+#Invoke-Command -ScriptBlock $ScriptBlock -ArgumentList ($args + @('someargument'))
--- AppVeyor/RunTests.ps1
+++ AppVeyor/RunTests.ps1
@@ -1,11 +1,10 @@
-﻿#run tests
+﻿# run tests
 dotnet test "--logger:trx;LogFileName=results.trx"
 
 # upload results to AppVeyor
 $files = Get-ChildItem -Path ".\" -Recurse -Include *.trx
 
-$files | Foreach-Object 
-{
+$files | Foreach-Object {
 	$wc = New-Object 'System.Net.WebClient'
 	$wc.UploadFile("https://ci.appveyor.com/api/testresults/mstest/$($env:APPVEYOR_JOB_ID)", $_.FullName)
 }
