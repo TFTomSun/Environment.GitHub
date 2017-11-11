a1c11f087f977e0fb1c03d0f4b8df347b0a985be
TomSun <tftomsun@web.de>
2017-11-11 17:22:11 +0100

    extracted powershell scripts from default.yml

---
+++ AppVeyor/InvokeWebScript.ps1
@@ -0,0 +1,5 @@
+﻿param([String]$scriptFileName) #Must be the first statement in your script
+
+$Script = Invoke-WebRequest 'https://raw.githubusercontent.com/TFTomSun/Environment.GitHub/master/AppVeyor/' + $scriptName;
+$ScriptBlock = [Scriptblock]::Create($Script.Content)
+Invoke-Command -ScriptBlock $ScriptBlock -ArgumentList ($args + @('someargument'))
---
+++ AppVeyor/RunTests.ps1
@@ -0,0 +1,11 @@
+﻿#run tests
+dotnet test "--logger:trx;LogFileName=results.trx"
+
+# upload results to AppVeyor
+$files = Get-ChildItem -Path ".\" -Recurse -Include *.trx
+
+$files | Foreach-Object 
+{
+	$wc = New-Object 'System.Net.WebClient'
+	$wc.UploadFile("https://ci.appveyor.com/api/testresults/mstest/$($env:APPVEYOR_JOB_ID)", $_.FullName)
+}
