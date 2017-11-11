fcda2bf95a7aeb2c33438560eb210ad6cc9e3d89
TomSun <tftomsun@web.de>
2017-11-11 17:27:00 +0100

    fixed web script

--- AppVeyor/InvokeWebScript.ps1
+++ AppVeyor/InvokeWebScript.ps1
@@ -1,5 +1,6 @@
 ﻿param([String]$scriptFileName) #Must be the first statement in your script
 
-$Script = Invoke-WebRequest 'https://raw.githubusercontent.com/TFTomSun/Environment.GitHub/master/AppVeyor/' + $scriptName;
+$url = 'https://raw.githubusercontent.com/TFTomSun/Environment.GitHub/master/AppVeyor/' + $scriptFileName;
+$Script = Invoke-WebRequest $url;
 $ScriptBlock = [Scriptblock]::Create($Script.Content)
 Invoke-Command -ScriptBlock $ScriptBlock -ArgumentList ($args + @('someargument'))
