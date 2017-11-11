a31ca4a473c2375239fa3c45d1fff87ddf03bac8
TomSun <tftomsun@web.de>
2017-11-11 18:01:47 +0100

    added readme update to default build script

---
+++ AppVeyor/UpdateReadme.ps1
@@ -0,0 +1,26 @@
+﻿git config --global credential.helper store
+git config --global user.email "no@mail.com"
+git config --global user.name "AppVeyor Agent"
+echo env %env.access_token2%
+echo pure %access_token2%
+ps: Add-Content "$env:USERPROFILE\.git-credentials" "https://$($env:access_token2):x-oauth-basic@github.com`n"
+type %USERPROFILE%\.git-credentials
+echo Tralala
+echo "APPVEYOR_PROJECT_ID %APPVEYOR_PROJECT_ID%"
+echo Tralala2
+echo "APPVEYOR_PROJECT_NAME %APPVEYOR_PROJECT_NAME%"
+echo "APPVEYOR_ACCOUNT_NAME %APPVEYOR_ACCOUNT_NAME%"
+echo "APPVEYOR_PROJECT_SLUG %APPVEYOR_PROJECT_SLUG%"
+
+echo "read README.content.md" 
+$readMe = Get-Content README.content.md
+      
+echo "read build status header file"     
+$header = ((Invoke-webrequest -URI "https://raw.githubusercontent.com/TFTomSun/Environment.GitHub/master/BuildStatus.template.md").Content).replace("[APPVEYOR_REPO_NAME]",$($env:APPVEYOR_REPO_NAME)).replace("[APPVEYOR_PROJECT_NAME]", $($env:APPVEYOR_PROJECT_NAME)).replace("[APPVEYOR_BUILD_VERSION]", $($env:APPVEYOR_BUILD_VERSION)).replace("[APPVEYOR_PROJECT_SLUG]", $($env:APPVEYOR_PROJECT_SLUG)).replace("[APPVEYOR_ACCOUNT_NAME]", $($env:APPVEYOR_ACCOUNT_NAME))
+
+echo "Build status header is : $header"
+Set-Content README.md –value $header, $readMe
+
+git add -A
+git commit -m "[skip ci] README.md updated by AppVeyor build"
+git push origin HEAD:%APPVEYOR_REPO_BRANCH%
