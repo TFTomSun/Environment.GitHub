93d37768844147a900db96edd88041269c0fc435
TomSun <tftomsun@web.de>
2017-11-12 16:57:24 +0100

    updated scripts

--- AppVeyor/GetReferencingPackages.ps1
+++ AppVeyor/GetReferencingPackages.ps1
@@ -2,12 +2,15 @@
 
 $PackageId = $nugetPackageName
 
-$wr = Invoke-WebRequest "http://packages.nuget.org/v1/FeedService.svc/Packages?`$filter=substringof(%27$PackageId%27,%20Dependencies)%20eq%20true&`$select=Id,Version,Dependencies"
+$url = "http://packages.nuget.org/v1/FeedService.svc/Packages?`$filter=substringof(%27$PackageId%27,%20Dependencies)%20eq%20true&`$select=Id,Version,Dependencies"
+echo $url
+$wr = Invoke-WebRequest $url
+
 if ($wr.StatusCode -ne 200) {
     Write-Error $wr.StatusDescription
     return
 }
-
+echo $wr
 $xml = [xml]$wr.content
 $xml.feed.entry.properties
 
---
+++ AppVeyor/ScriptPlayground.ps1
@@ -0,0 +1,36 @@
+﻿
+function RunLocalScript
+{
+ Param(
+ [parameter(Mandatory=$true)][String]$scriptFilePath,
+ [parameter(ValueFromRemainingArguments=$true)][String[]]$arguments
+    )
+
+# Invoke the script
+Invoke-Expression "$scriptFilePath $arguments"
+}
+
+function RunWebScript
+{
+ Param(
+ [parameter(Mandatory=$true)][String]$scriptFileName,
+ [parameter(ValueFromRemainingArguments=$true)][String[]]$arguments
+    )
+
+$url = 'https://raw.githubusercontent.com/TFTomSun/Environment.GitHub/master/AppVeyor/' + $scriptFileName;
+$scriptFilePath = [io.path]::combine($($env:TEMP), $scriptFileName)
+# Download the script
+Invoke-WebRequest -Uri $url -OutFile $scriptFilePath
+# Invoke the script
+Invoke-Expression "$scriptFilePath $arguments"
+}
+
+Push-Location $MyInvocation.MyCommand.Path
+[Environment]::CurrentDirectory = $PWD
+
+
+RunLocalScript ".\GetReferencingPackages.ps1" "Newtonsoft.Json"
+
+##  Your Script Code ...
+#Pop-Location
+#[Environment]::CurrentDirectory = $PWD
