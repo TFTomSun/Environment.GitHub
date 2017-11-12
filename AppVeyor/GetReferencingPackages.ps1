126238abfe745556edb075a84e67a0f5030133ad
TomSun <tftomsun@web.de>
2017-11-12 02:20:06 +0100

    added GetReferencingPackages.ps1

---
+++ AppVeyor/GetReferencingPackages.ps1
@@ -0,0 +1,13 @@
+﻿param([String]$nugetPackageName) #Must be the first statement in your script
+
+$PackageId = $nugetPackageName
+
+$wr = Invoke-WebRequest "http://packages.nuget.org/v1/FeedService.svc/Packages?`$filter=substringof(%27$PackageId%27,%20Dependencies)%20eq%20true&`$select=Id,Version,Dependencies"
+if ($wr.StatusCode -ne 200) {
+    Write-Error $wr.StatusDescription
+    return
+}
+
+$xml = [xml]$wr.content
+$xml.feed.entry.properties
+
